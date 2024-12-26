1373af1e03bf05133b73d9417b7892ac404c9b5b
Christ <class30831@gmail.com>
2024-12-26 09:42:04 +0800

    Vue3 config setting

---
+++ stock_frontend/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="app">
+        <h1>Hello Vue 3</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App'
+}
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    text-align: center;
+    margin-top: 60px;
+}
+</style>
